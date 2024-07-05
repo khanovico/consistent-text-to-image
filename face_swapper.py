4f639fa772606197a3c24f272dbe11ba1daa895f
KhanovicAI <khanovicDev@gmail.com>
2024-07-05 21:35:48 +0300

    fix: swapper comment

--- face_swapper.py
+++ face_swapper.py
@@ -22,6 +22,7 @@ def swap_one_image(src_img: str, target_face: str, out: str):
          target_face: target face image location
          out: save location of generated image
 
+
   """
   swapped_img = f2f_engine.swap_one_image(cv2.imread(src_img), cv2.imread(target_face))
   cv2.imwrite(out, swapped_img)
